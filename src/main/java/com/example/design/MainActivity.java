acf9bad160ee020f4bfde0f3c506118d73f9eed8
Raveesh <raveeshtiwari.121707@marwadiuniversity.ac.in>
2026-01-06 16:53:05 +0530

    add navigation

--- src/main/java/com/example/design/MainActivity.java
+++ src/main/java/com/example/design/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.design;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the button we made in the XML
+        View btnAdd = findViewById(R.id.btnAddJournal);
+
+        // Tell it to open the AddJournalActivity when clicked
+        btnAdd.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddJournalActivity.class);
+            startActivity(intent);
+        });
     }
 }
